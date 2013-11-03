236eb8e7463624d56129ce11c78e2677a437c5eb
cmastudios <admin@cmastudios.me>
2013-11-03 01:31:24 -0500

    Added background and player movement

--- src/main/java/me/cmastudios/zerohour/Game.java
+++ src/main/java/me/cmastudios/zerohour/Game.java
@@ -1,10 +1,6 @@
 package me.cmastudios.zerohour;
 
-import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
+import org.newdawn.slick.*;
 
 /**
  * A game using Slick2d
@@ -16,26 +12,43 @@ public class Game extends BasicGame {
     /** Screen height */
     private static final int HEIGHT = 600;
     
-    /** A counter... */
-    private int counter;
+    private int playerPosX;
+    private int playerPosY;
+    private Image playerImage;
+    private Image background;
 
     public Game() {
-        super("A Slick2d game");
+        super("Super Soccer Lineup");
     }
 
     public void render(GameContainer container, Graphics g) throws SlickException {
-        g.drawString("Hello, " + Integer.toString(counter) + "!", 50, 50);
+        g.setBackground(Color.black);
+        g.drawImage(background, 0, 0);
+        g.drawImage(playerImage, playerPosX, playerPosY);
 
     }
 
     @Override
     public void init(GameContainer container) throws SlickException {
-        counter = 0;
+        playerPosX = 400;
+        playerPosY = 300;
+        playerImage = new Image(Game.class.getClassLoader().getResourceAsStream("player.png"), "player", false);
+        background = new Image(Game.class.getClassLoader().getResourceAsStream("field.png"), "background", false);
+        container.setVSync(true);
     }
 
     @Override
     public void update(GameContainer container, int delta) throws SlickException {
-        counter++;
+        if (container.getInput().isKeyDown(Input.KEY_A)) {
+            playerPosX -= 2;
+        } else if (container.getInput().isKeyDown(Input.KEY_D)) {
+            playerPosX += 2;
+        }
+        if (container.getInput().isKeyDown(Input.KEY_W)) {
+            playerPosY -= 2;
+        } else if (container.getInput().isKeyDown(Input.KEY_S)) {
+            playerPosY += 2;
+        }
     }
     
     public static void main(String[] args) throws SlickException {
